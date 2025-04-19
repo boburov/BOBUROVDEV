94b323f8fd87bb0d26e7918a15b476911b7eac4a
Boburov-Shukurillo <boburovshukurilo@gmail.com>
2025-04-19 15:50:14 +0500

    new skills added

--- src/pages/Ability.jsx
+++ src/pages/Ability.jsx
@@ -10,20 +10,28 @@ import swiper_icon from "../assets/icons/swiper.png";
 import gsap_icon from "../assets/icons/gsap.jpg";
 import git_icon from "../assets/icons/git.png";
 import mui_icon from "../assets/icons/mui.png";
+import typescriptic from "../assets/icons/typescript.png";
+import npmic from "../assets/icons/npm.png";
+import nestjs from "../assets/icons/nestjs.webp";
+import nextjs from "../assets/icons/nextjs.webp";
 
 const Ability = () => {
   const skill_imgs = [
-    { name: "html", img_name: html_icon, isFull: true },
-    { name: "css", img_name: css_icon, isFull: true },
-    { name: "tailwind", img_name: tailwind_icon, isFull: true },
-    { name: "js", img_name: js_icon, isFull: true },
-    { name: "react", img_name: react_icon, isFull: true },
-    { name: "nodejs", img_name: nodejs_icon, isFull: false },
-    { name: "leaft", img_name: leaft_icon, isFull: true },
-    { name: "gsap", img_name: gsap_icon, isFull: false },
-    { name: "swiper", img_name: swiper_icon, isFull: true },
-    { name: "git", img_name: git_icon, isFull: true },
-    { name: "mui", img_name: mui_icon, isFull: true },
+    { name: "html", img_name: html_icon, isCompleated: true },
+    { name: "css", img_name: css_icon, isCompleated: true },
+    { name: "tailwind", img_name: tailwind_icon, isCompleated: true },
+    { name: "js", img_name: js_icon, isCompleated: true },
+    { name: "react", img_name: react_icon, isCompleated: true },
+    { name: "nodejs", img_name: nodejs_icon, isCompleated: false },
+    { name: "leaft", img_name: leaft_icon, isCompleated: true },
+    { name: "gsap", img_name: gsap_icon, isCompleated: false },
+    { name: "swiper", img_name: swiper_icon, isCompleated: true },
+    { name: "git", img_name: git_icon, isCompleated: true },
+    { name: "mui", img_name: mui_icon, isCompleated: true },
+    { name: "typescript", img_name: typescriptic, isCompleated: false },
+    { name: "npm", img_name: npmic, isCompleated: true },
+    { name: "next js", img_name: nextjs, isCompleated: false },
+    { name: "nest js", img_name: nestjs, isCompleated: false },
   ];
 
   return (
@@ -43,12 +51,12 @@ const Ability = () => {
                   src={elem.img_name}
                   alt={elem.name + " icon"}
                 />
-                {!elem.isFull && (
+                {!elem.isCompleated && (
                   <span className="montbold absolute w-full h-6 flex items-center justify-center bg-black/70 text-white z-10 top-5 -right-6 text-center border-y-2 rotate-45 text-sm">
                     in process
                   </span>
                 )}
-                {elem.isFull && (
+                {elem.isCompleated && (
                   <span className="montbold absolute w-full h-6 flex items-center justify-center bg-green-700/80 text-white z-10 top-5 -right-6 text-center border-y-2 rotate-45 text-sm">
                     success
                   </span>
@@ -60,12 +68,18 @@ const Ability = () => {
         </div>
 
         <div className="w-1/2 flex flex-col items-start">
-          <h2 data-aos="fade-up" 
-          data-aos-duration="300" className="text-3xl montbold border-b-2 mb-6">
+          <h2
+            data-aos="fade-up"
+            data-aos-duration="300"
+            className="text-3xl montbold border-b-2 mb-6"
+          >
             Education
           </h2>
-          <p data-aos="fade-up" 
-          data-aos-duration="350" className="text-gray-700">
+          <p
+            data-aos="fade-up"
+            data-aos-duration="350"
+            className="text-gray-700"
+          >
             I started learning IT in 2021, and to this day, I continue improving
             my frontend skills. I have a strong passion for{" "}
             <strong>
@@ -76,21 +90,27 @@ const Ability = () => {
           </p>
 
           <p
-            data-aos="fade-up" 
+            data-aos="fade-up"
             data-aos-duration="400"
             className="text-xl font-semibold text-center text-gray-900"
           >
             <em>"Big dreams need big sacrifices."</em>
           </p>
 
-          <p data-aos="fade-up" 
-          data-aos-duration="450" className="text-gray-700 mb-4">
+          <p
+            data-aos="fade-up"
+            data-aos-duration="450"
+            className="text-gray-700 mb-4"
+          >
             This pushes me to start learning something new, whether it's{" "}
             <strong>React</strong> or another programming language.
           </p>
 
-          <p  data-aos="fade-up" 
-          data-aos-duration="500" className="text-gray-700 mb-3">
+          <p
+            data-aos="fade-up"
+            data-aos-duration="500"
+            className="text-gray-700 mb-3"
+          >
             I especially love 3D web design because it looks stunning, modern,
             and premium. Even though it was harder to learn compared to other
             frameworks, I kept going, and now it feels much easier for me. I
@@ -98,8 +118,11 @@ const Ability = () => {
             part of my journey.
           </p>
 
-          <p  data-aos="fade-up" 
-          data-aos-duration="550" className="text-gray-700">
+          <p
+            data-aos="fade-up"
+            data-aos-duration="550"
+            className="text-gray-700"
+          >
             It took me <strong>two years</strong> to land my first job in this
             field. This is a short summary of my education history, and one day,
             I believe{" "}
