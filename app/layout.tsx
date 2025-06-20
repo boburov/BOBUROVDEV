a392749bc94dfbd39d79f30d9386a7640c23e4b3
Boburov-Shukurillo <boburovshukurilo@gmail.com>
2025-06-20 17:50:24 +0500

    added meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,17 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "B.Shukurullo • Software Craftsman",
-   description: "Official portfolio of Shukurullo Boburov, a fullstack JavaScript developer."
+  description:
+    "Official portfolio of Shukurullo Boburov, a fullstack JavaScript developer.",
+  other: {
+    "google-site-verification": "MaugEz5g-WDQQUU6ft4Q4Lf5dFcTSjwdDyJA1UKPo64",
+  },
+  openGraph: {
+    title: "B.Shukurullo • Software Craftsman",
+    url: "https://www.boburov.uz",
+    siteName: "boburov.uz",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
